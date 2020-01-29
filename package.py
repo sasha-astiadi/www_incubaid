d9936858b8d719dbe9d63b1bd59c01e44d960d45
samir <hossnys@greenitglobe.com>
2020-01-29 18:36:32 +0200

    Update package.py

--- package.py
+++ package.py
@@ -1,18 +1,28 @@
 from Jumpscale import j
 
-class Package(j.baseclasses.threebot_package):
 
+class Package(j.baseclasses.threebot_package):
+    """
+    JSX> cl = j.servers.threebot.local_start_zerobot(background=False)
+    JSX> cl = j.clients.gedis.get("abc", port=8901, package_name="zerobot.packagemanager")
+    JSX> cl.actors.package_manager.package_add(git_url="https://github.com/threefold-love/www_incubaid_com/tree/3bot")
+    """
+    DOMAIN = "www.incubaid.com"
     def start(self):
         server = self.openresty
         server.configure()
-        for port in [80, 443]:
-            website = server.get_from_port(port)
-            website.domain = "incubaid.com"
-            locations = website.locations.get(f"incubaid_locations_{port}")
+        website_incubaid = server.websites.get("www_incubaid_com")
+        website_incubaid.domain = self.DOMAIN
+        website_incubaid.port = 80
+        website_incubaid.ssl = False
+
+        websites = [server.get_from_port(80), server.get_from_port(443), website_incubaid]
+        for website in websites:
+            locations = website.locations.get(f"3bot_locations_{website.name}")
 
             website_location = locations.locations_static.new()
-            website_location.name = "incubaidwebsite"
-            website_location.path_url = "/"
+            website_location.name = "incubaid_website"
+            website_location.path_url = "/" if website.domain == self.DOMAIN else "/incubaid_com"
             fullpath = j.sal.fs.joinPaths(self.package_root, "html/")
             website_location.path_location = fullpath
 
